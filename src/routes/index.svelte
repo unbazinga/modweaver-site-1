fb03891b265009d05528818f5b0ab7d177c89ba4
ExoPlant <lennyj2468+la@gmail.com>
2022-02-05 08:23:47 +0000

    Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,50 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<body>
+<nav>
+<!--<p class="name"><img src="/favicon.png" width="100" height="56.25">modweaver</p>-->
+
+    <div style="text-align: center;">
+
+        <div class="flex-container">
+            <div class="c"><img src="/icon_text.png" width="130" height="65"/></div>
+            <div class="c"><a class="c" href="/">Home</a></div>
+            <div class="c"><a class="c" href="/mods">Mods</a></div>
+        </div>
+    </div>
+
+</nav>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the >documentation</p>
+<style>
+    body {
+        background-color: #21252b;
+    }
+    * {
+        color: white;
+    }
+    nav {
+        margin-left: 42.5%;
+        margin-right: 50%;
+        font-size: 15px;
+    }
+    .name {
+        font-weight: bold;
+
+    }
+    .c {
+        text-align: center;
+        font-size: 15px;
+    }
+    .flex-container {
+        display: flex;
+    }
+
+    .flex-container > div {
+
+        margin: 5px;
+        padding: 1px;
+        font-size: 30px;
+    }
+    /* Add a black background color to the top navigation */
+</style>
+</body>
---
+++ src/routes/mods.svelte
@@ -0,0 +1,41 @@
+<head>
+    <!--<style>
+        body {
+            background-color: #21252b;
+        }
+        * {
+            color: white;
+        }
+        nav {
+            margin-left: 42.5%;
+            margin-right: 50%;
+            font-size: 15px;
+        }
+        .name {
+            font-weight: bold;
+
+        }
+        .c {
+            text-align: center;
+            font-size: 15px;
+        }
+        .flex-container {
+            display: flex;
+        }
+
+        .flex-container > div {
+
+            margin: 5px;
+            padding: 1px;
+            font-size: 30px;
+        }
+        </style>
+        -->
+    <link rel="stylesheet" href="/style.css">
+    <title>ModWeaver Mods</title>
+</head>
+<body>
+
+<h1>Mods</h1>
+
+</body>
