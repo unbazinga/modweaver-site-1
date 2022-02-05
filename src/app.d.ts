5f207d2a8a00cbf269a632955fdc5aa9a9ba9e77
ExoPlant <lennyj2468+la@gmail.com>
2022-02-05 17:22:10 +0000

    Add mysql test

--- src/app.d.ts
+++ src/app.d.ts
@@ -10,4 +10,21 @@ declare namespace App {
 	interface Session {}
 
 	interface Stuff {}
+	let mysql = require('mysql');
+	let sql_password = process.SQL_PASSWORD;
+	let sql_user = process.SQL_USER;
+	let sql_host = process.SQL_HOST;
+	let connection = mysql.createConnection({
+		host: sql_host,
+		user: sql_user,
+		password: sql_password,
+		database: 'modweaver_staging'
+	});
+	connection.connect(function(err) {
+		if (err) {
+			return console.error('error: ' + err.message);
+		}
+
+		console.log('Connected to the MySQL server.');
+	});
 }
