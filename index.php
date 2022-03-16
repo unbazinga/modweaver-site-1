3cf7d605963042eefaadf35eac4932df9e95fb47
Mathew Scott-Halliday <sm8079@tmc.catholic.edu.au>
2022-03-16 12:45:17 +1030

    Adding navbar.php + Fixing mod page title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include("./includes/navbar.php"); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ModWeaver</title>
+    <title>ModWeaver - Home</title>
     <link rel="stylesheet" href="style.css">
   </head>
     
